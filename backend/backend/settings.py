57d7f9570c29461906df21006db7eab56e253643
jateen1906-ship-it <jateen1906@gmail.com>
2026-04-28 12:03:07 +0530

    Move email settings to environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,8 +132,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jateenrane19@gmail.com'
-EMAIL_HOST_PASSWORD = 'qbmm ecoq iklf gvnu'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'jateenrane19@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'qbmm ecoq iklf gvnu')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 
 
