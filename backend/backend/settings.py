ca131860d416828185f5d7a5eee8dd766ce220ee
jateen1906-ship-it <jateen1906@gmail.com>
2026-04-29 02:08:12 +0530

    email credential change in setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,8 +130,9 @@ AUTH_USER_MODEL = 'accounts.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'jateen1906@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'seqz qrrr umzx yfss')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
