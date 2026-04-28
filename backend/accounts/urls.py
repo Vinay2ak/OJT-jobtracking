1def9dd22b87bf4041aec72c9e504245ec0c402f
jateen1906-ship-it <jateen1906@gmail.com>
2026-04-28 17:25:49 +0530

    backend deleted from main

--- backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import SignupView, SendOTPView, VerifyOTPView, LoginWithOTPView
-
-urlpatterns = [
-    path('signup/', SignupView.as_view()),
-    path('send-otp/', SendOTPView.as_view()),
-    path('verify-otp/', VerifyOTPView.as_view()),
-    path('login-otp/', LoginWithOTPView.as_view()),
-]
